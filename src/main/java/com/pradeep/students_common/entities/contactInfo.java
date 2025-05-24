dca7355cc5c13e8f710c2045d804c5a99e42f059
pradeep kumar patel <pradeepkumarpatel1998@gmail.com>
2025-05-24 22:17:15 +0530

    new chanes related to enetities

--- src/main/java/com/pradeep/students_common/entities/contactInfo.java
+++ src/main/java/com/pradeep/students_common/entities/contactInfo.java
@@ -1,34 +1,29 @@
 package com.pradeep.students_common.entities;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
 
-@Document(collection = "books")
+@Document
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class books {
-    @Id
-    private String id;
+public class contactInfo {
+
+    private String Id;
 
     @DBRef
     private students Student;
 
-    private String title;
-
-    private String author;
-
-    private String isbn;
-
-    private double price;
-
-    private int publicationYear;
-
-    private String ShortDescription;
+    private String First_Name;
+    private String Middle_Name;
+    private String Last_Name;
+    private List<String> contact_Numbers;
+    private String Email;
+    private String Feild;
 
 }
---
+++ src/main/java/com/pradeep/students_common/entities/students.java
@@ -0,0 +1,36 @@
+package com.pradeep.students_common.entities;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class students {
+    @Id
+    private String StudentId;
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String contactNumber;
+    private String Age;
+
+
+    @DBRef(lazy = true)
+    private List<books> bookss;
+
+    @DBRef(lazy = true)
+    private List<contactInfo> contactInfos;
+
+    // Additional fields can be added as needed
+}
