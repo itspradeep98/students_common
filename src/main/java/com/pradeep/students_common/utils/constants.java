2d64741f4e954227c795af6218557d95a6c8a172
pradeep kumar patel <pradeepkumarpatel1998@gmail.com>
2025-06-15 02:13:08 +0530

    exception constants

--- src/main/java/com/pradeep/students_common/utils/constants.java
+++ src/main/java/com/pradeep/students_common/utils/constants.java
@@ -2,4 +2,9 @@ package com.pradeep.students_common.utils;
 
 public abstract class constants {
     public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+    public static final String ALARM_NOT_FOUND = "Alarm not found";
+    public static final String CONTACT_NOT_FOUND = "Contact not found";
+
+    public static final String CONSTANT_1 = "1";
+    public static final String CONSTANT_2 = "2";
 }
